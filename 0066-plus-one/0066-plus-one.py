d593f448860c1a11b77ba4a73b896a4965dc2b8b
Isabella Correa <Isabellapc97@gmail.com>
2026-02-04 21:20:48 -0500

    Time: 0 ms (100%), Space: 12.4 MB (87.23%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in range(len(digits) - 1, -1,-1):
+            total = digits[i] + carry
+            digits[i] = total % 10
+            carry = total // 10
+        if carry:
+            digits = [1] + digits
+        return digits
